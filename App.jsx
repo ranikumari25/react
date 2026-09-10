a26977d828cae0784f5d304da57453026ff731f8
ranikumari25 <ranikumari25@navgurukul.org>
2026-09-10 09:28:12 -0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+function app(){
+  return ( 
+  <>
+    <h1>Hello React</h1>
+
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </>
+  )
+  
+}
+
+export default app
