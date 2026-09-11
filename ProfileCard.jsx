883126fcb0a4b495cf8e5f3f6af9e475171fe1ea
ranikumari25 <ranikumari25@navgurukul.org>
2026-09-10 22:09:37 -0800

    Add files via upload

---
+++ ProfileCard.jsx
@@ -0,0 +1,12 @@
+function ProfileCard(){
+  return ( 
+   <div className="profile-card" style={{backgroundcolor: "#f4f4f4;"}}>
+<img src="profile.jpg" alt="User Profile"/>
+<h2>Rani Kumari</h2>
+<p className="bio">Software Developer</p> 
+</div>
+  )
+
+}
+
+export default ProfileCard;
